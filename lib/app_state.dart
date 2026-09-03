03f8377b3be9222442941073c2eab132accdd61d
Dada <63845798+andreadada@users.noreply.github.com>
2026-09-03 17:15:32 +0200

    fix: disambiguate finance Category model

--- lib/app_state.dart
+++ lib/app_state.dart
@@ -1,6 +1,6 @@
 import 'dart:math' as math;
 
-import 'package:flutter/foundation.dart';
+import 'package:flutter/foundation.dart' hide Category;
 
 import 'data/app_database.dart';
 import 'models/models.dart';
