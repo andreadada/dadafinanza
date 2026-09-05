366d881dabd1f87dabb5fd0d6957f01c5e431865
Dada <63845798+andreadada@users.noreply.github.com>
2026-09-05 05:09:58 +0200

    build: compile Android app against SDK 37

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.dadafinanza.app"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 37
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
